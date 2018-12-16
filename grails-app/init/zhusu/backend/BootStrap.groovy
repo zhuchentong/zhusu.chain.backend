3fc05b5e8e347ff7058bd7325ae2c54f9bae5a3f
foxgem <jianhgreat@hotmail.com>
2018-12-16 22:07:42 +0800

    修改order无法正常创建错误，同时增加开发环境初始化数据功能。

--- grails-app/init/zhusu/backend/BootStrap.groovy
+++ grails-app/init/zhusu/backend/BootStrap.groovy
@@ -2,13 +2,31 @@ package zhusu.backend
 
 import groovy.util.logging.Slf4j
 import org.springframework.boot.info.GitProperties
+import zhusu.backend.user.Role
+import zhusu.backend.user.User
+import zhusu.backend.user.UserService
 
 @Slf4j
 class BootStrap {
     GitProperties gitProperties
+    UserService userService
 
     def init = { servletContext ->
-        log.info("Application running at commit: ${gitProperties.shortCommitId}, branch: ${gitProperties.branch}, commit time: ${gitProperties.commitTime}, build time: ${gitProperties.getDate('build.time')}")
+        log.info("Application running at commit: {}, branch: {}, commit time: {}, build time: {}"
+                , gitProperties.shortCommitId
+                , gitProperties.branch
+                , gitProperties.commitTime
+                , gitProperties.getDate('build.time'))
+
+        environments {
+            development {
+                Role.validRoles().each {
+                    new Role(authority: it).save()
+                }
+                User user = new User(username: '13572209183', password: 'admin', displayName: 'admin')
+                userService.createUserWithRole(user, 'ROLE_ADMIN')
+            }
+        }
     }
     def destroy = {
     }
